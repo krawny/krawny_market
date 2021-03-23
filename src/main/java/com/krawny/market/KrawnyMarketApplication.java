83af6f6058a6f6ed0584bb371bd46ab336923797
Kr4wny <kr4wny@gmail.com>
2021-03-23 17:17:30 -0500

    primer commit

---
+++ src/main/java/com/krawny/market/KrawnyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.krawny.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrawnyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrawnyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krawny/market/KrawnyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krawny.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrawnyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
